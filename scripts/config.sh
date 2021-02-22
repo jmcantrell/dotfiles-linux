4203f579fe0dfed171dc685ba30f7b9c4ada7924
Jeremy Cantrell <jmcantrell@gmail.com>
2021-02-21 23:41:09 -0600

    config changes

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# only if allowed to use sudo
-if groups | grep -wq wheel; then
-
-    # disable pc speaker
-    echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/pcspkr.conf
-
-fi
-
 # ca certs can be in different places, so link to a common place
 mkdir -p ~/.local/etc/ssl/certs
 ln -sfv /etc/ssl/certs/ca-certificates.crt ~/.local/etc/ssl/certs/ca-certificates.crt
