8edfef80981108ecc806c8587dcdb3ee4a0a25ae
Jeremy Cantrell <jmcantrell@gmail.com>
2021-06-15 23:29:46 -0500

    Config changes

--- install.sh
+++ install.sh
@@ -2,23 +2,10 @@
 
 set -e
 
-vmsdir=$HOME/.virtualbox
-mkdir -p "$vmsdir"
-# disable btrfs copy-on-write for vms folder
-fstype=$(mount | grep " $(dirname "$HOME") " | cut -d' ' -f5)
-[[ "$fstype" = btrfs ]] && chattr +C "$vmsdir"
-
-vboxmanage setproperty machinefolder "$vmsdir"
-
-# ca certs can be in different places, so link to a common place
-mkdir -p ~/.local/etc/ssl/certs
-ln -sfv /etc/ssl/certs/ca-certificates.crt ~/.local/etc/ssl/certs/ca-certificates.crt
-
-# friendlier access to udiskie removable disks
-ln -svfT "/run/media/$USER" "$HOME/.local/media"
-
 rsync -av ./copy/ "$HOME"
 
+# If the user directories don't already exist, xdg-user-dirs-update will just
+# change the setting in the config file to $HOME, so this will create them.
 grep "^XDG_" ~/.config/user-dirs.dirs | cut -d= -f1 | while read -r name; do
     mkdir -p "$(xdg-user-dir "$name")"
 done
