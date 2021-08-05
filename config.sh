ff2ede36f351ec6a6ce6c31643bc51faa6566dca
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-05 05:51:11 -0500

    Moved from arch configs

--- config.sh
+++ config.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+sudo ufw enable
+sudo ufw default deny
+sudo ufw allow ssh
+sudo ufw allow mdns
+sudo ufw allow llmnr
+
 sudo gpasswd -a "$USER" video
 
 sudo chsh -s /bin/zsh "$USER"
+
+updatedb
