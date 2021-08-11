90dc2ddb4fc8adf817377aca6696e8ab99c0db3c
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-10 21:24:22 -0500

    Not sure -o pipefail is a good default

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eu
 
 # Needed to change screen brightness.
 sudo gpasswd -a "$USER" video
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eu
 
 git pull origin master
