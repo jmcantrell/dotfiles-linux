b54615641cf6ec03c075c8230365b7e8ce8b984c
Jeremy Cantrell <jmcantrell@gmail.com>
2021-11-01 01:29:43 -0500

    Add swaystatus module for showing systemd units

---
+++ stow/.local/etc/swaystatus/modules/systemd_unit.py
@@ -0,0 +1,42 @@
+from subprocess import DEVNULL, CalledProcessError
+from swaystatus import BaseElement
+from .util import run_safe
+from .colors import color_off
+
+
+class Element(BaseElement):
+    def __init__(self, *args, **kwargs):
+        self._unit = kwargs.pop("unit")
+        self._user = kwargs.pop("user", False)
+        self._label = kwargs.pop("label", self._unit)
+        super().__init__(*args, **kwargs)
+
+    def _systemctl(self, *args):
+        command = ["systemctl"]
+        if self._user:
+            command.append("--user")
+        command.extend(args)
+        return run_safe(command, stdout=DEVNULL)
+
+    def _is_active(self):
+        try:
+            self._systemctl("is-active", self._unit)
+            return True
+        except CalledProcessError:
+            return False
+
+    def on_update(self, output):
+        options = {}
+        if not self._is_active():
+            options["color"] = color_off
+
+        output.append(
+            self.create_block(
+                self._label,
+                instance=str(self._unit),
+                **options,
+            )
+        )
+
+    def on_click_1(self, event):
+        self._systemctl("stop" if self._is_active() else "start", self._unit)
