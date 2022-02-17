5928ea7b4627c101b7ec068c98f577580c70a569
Jeremy Cantrell <jmcantrell@gmail.com>
2022-02-17 17:53:31 -0600

    Add qutebrowser helpers and overrides

--- files/home/.config/qutebrowser/config.py
+++ files/home/.config/qutebrowser/config.py
@@ -1,1 +1,20 @@
+import os, logging
+
+logger = logging.getLogger("config")
+
+
+def boolenv(s):
+    return bool(int(s))
+
+
+def override(name, key, convert=str):
+    var = f"QUTEBROWSER_{name}"
+    if var in os.environ:
+        value = convert(os.environ[var])
+        logger.info(f"Overriding {key} with {value}")
+        config.set(key, value)
+
+
 config.load_autoconfig()
+
+override("DARKMODE", "colors.webpage.darkmode.enabled", convert=boolenv)
